039a255fcd07c05152d40e84e7262e777f5dc75e
Catherine Li <licatherine333@gmail.com>
2026-01-28 13:23:25 -0500

    save gamma output to json

--- src/retrieve_data/gamma.py
+++ src/retrieve_data/gamma.py
@@ -25,7 +25,9 @@ def save_raw_data(data, slug):
 if __name__ == "__main__":
 
     # Change this as needed
-    webpage_url = "https://polymarket.com/event/fed-decision-in-january"
+    # webpage_url = "https://polymarket.com/event/fed-decision-in-january"
+    # webpage_url = "https://polymarket.com/event/ecb-interest-rates-february-2026"
+    webpage_url = "https://polymarket.com/sports/nfl/games/week/15/nfl-sea-ne-2026-02-08"
 
     slug = webpage_url.split('/')[-1]
     url = f"https://gamma-api.polymarket.com/events/slug/{slug}"
